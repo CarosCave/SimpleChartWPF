b0adc4b0d42adb45d96e99562fd9ac5c59d17830
Caro <15948931+CarosCave@users.noreply.github.com>
2023-01-08 17:56:04 +0100

    Punkte zeichnen hinzugefügt

--- SimpleChartWPF/Punkte.cs
+++ SimpleChartWPF/Punkte.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace SimpleChartWPF
 {
@@ -15,7 +17,22 @@ namespace SimpleChartWPF
         }
         public override void Zeichne(Canvas canvas)
         {
-            //throw new NotImplementedException();
+            for (int i = 0; i < punkte.Count; i++)
+            {
+                double x = (punkte[i].Jahr - 1980.0) / (2014 - 1980) * canvas.ActualWidth;
+                double y = (1.0 - punkte[i].Wert / 300.0) * canvas.ActualHeight;
+
+                Ellipse elli = new Ellipse();
+                elli.Fill = Brushes.Red;
+
+                elli.Width = 10.0;
+                elli.Height = 10.0;
+
+                canvas.Children.Add(elli);
+
+                Canvas.SetLeft(elli, x - 5.0);
+                Canvas.SetTop(elli, y - 5.0);
+            }
         }
     }
 }
